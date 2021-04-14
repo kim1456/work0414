5db31386191d09682be670c0e0b397a296e8c93f
kim1456 <76077726+kim1456@users.noreply.github.com>
2021-04-14 10:45:25 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 class Hello{
   public static void main(String[] args){
-    printf("Hello");
+      //1~10까지 합계를 구하는 프로그램
+      int sum = 0;
+      for(int i = 1; i<10 ; i++){ 
+        sum + =i;
+      }
+      System.out.println(sum);
   }
 }
